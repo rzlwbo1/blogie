8ff62fc91d24f5d41afb55b54e61d65531c3ccad
rzlwbo1 <rizalwb2@gmail.com>
2023-11-06 23:00:24 +0700

    add saving data tag

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        } 
+
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            // Manually get request from form
+            //var name = Request.Form["name"]; // name = value dalam attribue name html
+            //var displayName = Request.Form["displayName"];
+
+
+            // model binding pake request di parameter
+            //string name = addTagRequest.Name;
+            //string display = addTagRequest.DisplayName;
+
+            Tag newTag = new()
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            bloggieDbContext.Tags.Add(newTag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("Add");
         }
     }
 }
---
+++ Bloggie.Web/Models/ViewModel/AddTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloggie.Web.Models.ViewModel
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
